29e7a2b7fae76e29b9c15e4095a91505579f8d73
Robert-Espinoza <robert.espinoza.s@tecsup.edu.pe>
2023-09-15 23:52:58 -0500

    first commit

---
+++ Semana03/Program.cs
@@ -0,0 +1,93 @@
+﻿//Librerias del ADO .NET
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Semana03;
+
+class Program
+{
+    // Cadena de conexión a la base de datos
+    public static string connectionString = "Data Source=LAB1504-28\\SQLEXPRESS;Initial Catalog=Tecsup2023DB;User ID=usertecsup;Password=123456";
+
+
+    static void Main()
+    {
+
+    }
+
+    //De forma desconectada
+    private static DataTable Tecsup2023DB()
+    {
+        // Crear un DataTable para almacenar los resultados
+        DataTable dataTable = new DataTable();
+        // Crear una conexión a la base de datos
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            // Abrir la conexión
+            connection.Open();
+
+            // Consulta SQL para seleccionar datos
+            string query = "SELECT * FROM Trabajadores";
+
+            // Crear un adaptador de datos
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+
+            // Llenar el DataTable con los datos de la consulta
+            adapter.Fill(dataTable);
+
+            // Cerrar la conexión
+            connection.Close();
+
+        }
+        return dataTable;
+    }
+
+    //De forma conectada
+    private static List<Trabajadores> ListarTrabajadoresListaObjetos()
+    {
+        List<Trabajadores> trabajadores = new List<Trabajadores>();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            // Abrir la conexión
+            connection.Open();
+
+            // Consulta SQL para seleccionar datos
+            string query = "SELECT IdTRabajador,Nombre, Apellidos, Sueldo, FechadeNacimiento FROM Trabajadores";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    // Verificar si hay filas
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            // Leer los datos de cada fila
+
+                            trabajadores.Add(new Trabajadores
+                            {
+                                Id = (int)reader["IdTRabajador"],
+                                Nombre = reader["Nombre"].ToString(),
+                                Apellidos = reader["Apellidos"].ToString(),
+                                
+                            });
+
+                        }
+                    }
+                }
+            }
+
+            // Cerrar la conexión
+            connection.Close();
+
+
+        }
+        return trabajadores;
+
+    }
+
+
+}
---
+++ Semana03/Trabajadores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana03
+{
+    class Trabajadores
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public decimal Sueldo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
